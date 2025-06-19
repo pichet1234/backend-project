e1c7ecce40297d4a994fc136d41859e551217bb4
PichetDotK <146558868+pichet1234@users.noreply.github.com>
2025-06-19 11:36:23 +0700

    Update patient.js

--- routes/controller/patient.js
+++ routes/controller/patient.js
@@ -30,7 +30,7 @@ module.exports = {
             }else{
                 return res.status(400).json({
                     message: 'ท่านเคยลงทะเบียนแล้ว',
-                    patientId: existing?._id
+                    patientId: existing._id?.toString();
                 });
             }
     
