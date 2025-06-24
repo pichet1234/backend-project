d90bc5ee0a0dce3f7dde9ccc4a973a1986d63f6d
PichetDotK <pckjoke@gmail.com>
2025-06-24 14:23:56 +0700

    commit

--- routes/controller/patient.js
+++ routes/controller/patient.js
@@ -446,37 +446,39 @@ module.exports = {
         }
     },
     // followup
-    followupmild: async (req, res)=>{
-        try{
-            const result = await patient.aggregate([
-                {
-                    $lookup: {
-                      from: "assessment9q",
-                      localField: "_id",
-                      foreignField: "pid",
-                      as: "assessment"
-                    }
-                  },{
-                        $lookup:{
-                            from:"followup",
-                            localField:"_id",
-                            foreignField:"pid",
-                            as: "followup"
-                        }		
-                    },
-                  {
-                    $match: {
-                      "9Q": {
-                        $elemMatch: {
-                          score: { $gt: 7, $lte: 12 }
-                        }
-                      }
-                    }
-                  }                
-            ]);
-            res.json(result);
-        }catch(error){
-            res.status(500).json({ error:'เกิดข้อผิดผลาด' })
+    followupmild: async (req, res) => {
+        try {
+          const result = await patient.aggregate([
+            {
+              $lookup: {
+                from: "assessment9q",
+                localField: "_id",
+                foreignField: "pid",
+                as: "assessment"
+              }
+            },
+            {
+              $lookup: {
+                from: "followup",
+                localField: "_id",
+                foreignField: "pid",
+                as: "followup"
+              }
+            },
+            {
+              $match: {
+                assessment: {
+                  $elemMatch: {
+                    score: { $gt: 7, $lte: 12 }
+                  }
+                }
+              }
+            }
+          ]);
+          res.json(result);
+        } catch (error) {
+          res.status(500).json({ error: 'เกิดข้อผิดพลาด' });
         }
-    }
+      }
+      
 }
